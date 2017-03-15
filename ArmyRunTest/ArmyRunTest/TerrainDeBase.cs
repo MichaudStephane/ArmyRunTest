30e68ee038b5f71574cef09d1fc0c1020608ad3d
Stephame MIchaud <michaudstephane@yahoo.ca>
2017-03-15 10:01:28 -0400

    Jai fini la collision de base pouur les terrains de base

--- ArmyRunTest/ArmyRunTest/TerrainDeBase.cs
+++ ArmyRunTest/ArmyRunTest/TerrainDeBase.cs
@@ -37,27 +37,38 @@ namespace AtelierXNA
         public Vector3 DonnerVectorCollision(PrimitiveDeBaseAnimée a)
         {
             Vector3 vitesseTemp = (a as Soldat).Vitesse;
+
             Vector3 forceTemp = (a as Soldat).VecteurResultantForce;
             Vector3 v = Vector3.Zero;
             if (HitBoxGénérale.Intersects((a as Soldat).HitBoxGénérale))
-            {           
+            {
+
+                // c mon
+
                 v = a.Position - Position;
-                v = new Vector3(v.X / TAILLE_HITBOX_STANDARD.X, v.Y / TAILLE_HITBOX_STANDARD.Y*0.85f, (v.Z / TAILLE_HITBOX_STANDARD.Z)*0.85f);
+                v = new Vector3(v.X / (HOMOTHETHIE_STANDARD * TAILLE_HITBOX_STANDARD.X), v.Y / (HOMOTHETHIE_STANDARD * TAILLE_HITBOX_STANDARD.Y * 0.85f), (v.Z / HOMOTHETHIE_STANDARD * TAILLE_HITBOX_STANDARD.Z));
 
                 v = Convert.ToInt32((v.Y >= v.X) && (v.Y >= v.Z) && (vitesseTemp.Y < -0.0001f) && v.Y >= 0.0001f) * VECTOR_HAUT
+
                 + Convert.ToInt32((v.Y <= v.X) && (v.Y <= v.Z) && (vitesseTemp.Y > 0.0001f) && (v.Y <= -0.0001f)) * VECTOR_BAS
-                   + Convert.ToInt32((v.Z >= v.X) && (v.Z >= v.Y) && vitesseTemp.Z < -0.0001f) * VECTOR_DEVANT
-                //   + Convert.ToInt32(vitesseTemp.X >= 0.001f) * VECTOR_GAUCHE;
+
+                   + Convert.ToInt32((v.Z >= v.X) && (v.Z >= v.Y) && vitesseTemp.Z < -0.0001f && v.Z > 0.0001f) * VECTOR_DEVANT
+
+                   + Convert.ToInt32((vitesseTemp.X >= 0.001f) && (v.X <= -0.0001f) && (Math.Abs(v.X) >= Math.Abs(v.Y)) && (Math.Abs(v.X) >= Math.Abs(v.Z))) * VECTOR_GAUCHE
+
+                   + Convert.ToInt32(vitesseTemp.X <= -0.0001f && v.X >= 0.0001f && (Math.Abs(v.X) >= Math.Abs(v.Y)) && (Math.Abs(v.X) >= Math.Abs(v.Z))) * VECTOR_DROITE
+
+                   + Convert.ToInt32((Math.Abs(v.Z) >= Math.Abs(v.X)) && (Math.Abs(v.Z) >= Math.Abs(v.Y)) && vitesseTemp.Z > 0.0001f && v.Z < -0.0001f) * VECTOR_DERRIERE
                 ;
 
-                if(v.Y>0)
-                (a as Soldat).EstSurTerrain = true;
+                if (v.Y > 0)
+                    (a as Soldat).EstSurTerrain = true;
 
                 (a as Soldat).EstEnCollision = true;
 
                 (a as Soldat).Vitesse += new Vector3(v.X * Math.Abs(vitesseTemp.X), v.Y * Math.Abs(vitesseTemp.Y), v.Z * Math.Abs(vitesseTemp.Z));
-              //  v = new Vector3(v.X * forceTemp.X, v.Y * forceTemp.Y, v.Z * forceTemp.Z);
-            }         
+                //  v = new Vector3(v.X * forceTemp.X, v.Y * forceTemp.Y, v.Z * forceTemp.Z);
+            }
             return -v;
         }
         private float GarderHorsBornes(Soldat a,Vector3 v)
