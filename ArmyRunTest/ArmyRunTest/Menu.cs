f3a3d8e6de483dde7b8478c6b8e9f378aa91a606
alexfulldevil <alexfulldevil123@hotmail.com>
2017-04-10 13:55:24 -0400

    Fix hitbox sectionsNiveau

--- ArmyRunTest/ArmyRunTest/Menu.cs
+++ ArmyRunTest/ArmyRunTest/Menu.cs
@@ -48,56 +48,10 @@ namespace AtelierXNA
                 {
                     if (GestionnaireManager.EstNouveauClicGauche())
                     {
-                        SectionRepos test = new SectionRepos(Game, new Vector3(0, 0, 20), 1);
-                        SectionVentilateur test2 = new SectionVentilateur(Game, new Vector3(0, 0, -40), 2);
-                        SectionRepos test3 = new SectionRepos(Game, new Vector3(0, 0, 0), 1);
-                        SectionRepos test4 = new SectionRepos(Game, new Vector3(0, 0, -20), 1);
-                        SectionHache test5 = new SectionHache(Game, new Vector3(0, 0, -60), 1);
-                        SectionRepos test6 = new SectionRepos(Game, new Vector3(0, 0, 40), 1);
-                        SectionRepos test7 = new SectionRepos(Game, new Vector3(0, 0, -80), 1);
-
-                        List<PrimitiveDeBase>[] ObjetCollisionné = new List<PrimitiveDeBase>[1];
-                        List<PrimitiveDeBase>[] ObjetCollisionné2 = new List<PrimitiveDeBase>[1];
                         List<PrimitiveDeBase>[] temp = new List<PrimitiveDeBase>[1];
-                        temp[0] = new List<PrimitiveDeBase> { };
-                        for (int i = 0; i < test.GetListeCollisions().Count; i++)
-                        {
 
-                            temp[0].Add(test.GetListeCollisions()[i]);
-                        }
-                        for (int i = 0; i < test2.GetListeCollisions().Count; i++)
-                        {
-
-                            temp[0].Add(test2.GetListeCollisions()[i]);
-                        }
-                        for (int i = 0; i < test3.GetListeCollisions().Count; i++)
-                        {
-
-                            temp[0].Add(test3.GetListeCollisions()[i]);
-                        }
-                        for (int i = 0; i < test4.GetListeCollisions().Count; i++)
-                        {
-
-                            temp[0].Add(test4.GetListeCollisions()[i]);
-                        }
-                        for (int i = 0; i < test5.GetListeCollisions().Count; i++)
-                        {
-
-                            temp[0].Add(test5.GetListeCollisions()[i]);
-                        }
-                        for (int i = 0; i < test6.GetListeCollisions().Count; i++)
-                        {
-
-                            temp[0].Add(test6.GetListeCollisions()[i]);
-                        }
-                        for (int i = 0; i < test7.GetListeCollisions().Count; i++)
-                        {
-
-                            temp[0].Add(test7.GetListeCollisions()[i]);
-                        }
-
-                        ObjetCollisionné[0] = test.GetListeCollisions();
-                        Game.Components.Add(new Armée(Game, 50, new Vector3(0, 2, -60), Atelier.INTERVALLE_STANDARD, temp));
+                        List<SectionDeNiveau> a = null;
+                        Game.Components.Add(new Armée(Game, 50, new Vector3(0, 2, -60), Atelier.INTERVALLE_STANDARD, temp,a));
                         Game.Components.Add(CaméraJeuAutomate);
                         Game.Components.Add(new Afficheur3D(Game));
                         Game.Components.Add(new TuileTexturée(Game, 100F, Vector3.Zero, Vector3.Zero, new Vector2(1, 1), "FeuFollet", 1f / 60));
